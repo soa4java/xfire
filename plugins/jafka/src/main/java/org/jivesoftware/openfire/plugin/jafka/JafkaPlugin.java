6cb989260d810d6b2cc2746aa8a4e55dbd07a8f4
yanrc <yanrc@servyou.com.cn>
2015-11-01 22:49:38 +0000

    modify jafka plugin

--- plugins/jafka/src/main/java/org/jivesoftware/openfire/plugin/jafka/JafkaPlugin.java
+++ plugins/jafka/src/main/java/org/jivesoftware/openfire/plugin/jafka/JafkaPlugin.java
@@ -1,6 +1,7 @@
 package org.jivesoftware.openfire.plugin.jafka;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -9,15 +10,28 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.swing.text.Document;
+
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.XMPPPacketReader;
 import org.jivesoftware.of.common.plugin.PluginAdaptor;
 import org.jivesoftware.of.common.service.RestService;
 import org.jivesoftware.openfire.OfflineMessageListener;
 import org.jivesoftware.openfire.OfflineMessageStrategy;
+import org.jivesoftware.openfire.PacketRouter;
+import org.jivesoftware.openfire.XMPPServer;
 import org.jivesoftware.openfire.container.Plugin;
 import org.jivesoftware.openfire.container.PluginManager;
+import org.jivesoftware.openfire.net.MXParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 import org.xmpp.packet.Message;
+import org.xmpp.packet.Packet;
+import org.xmpp.packet.Presence;
 
 import com.sohu.jafka.consumer.Consumer;
 import com.sohu.jafka.consumer.ConsumerConfig;
@@ -31,10 +45,58 @@ import com.sohu.jafka.producer.serializer.StringEncoder;
 import com.sohu.jafka.utils.ImmutableMap;
 
 public class JafkaPlugin extends PluginAdaptor implements Plugin, OfflineMessageListener {
-	private Logger logger = LoggerFactory.getLogger(JafkaPlugin.class);
-	private Producer<String, String> producer;
+	private static Logger logger = LoggerFactory.getLogger(JafkaPlugin.class);
+	private static String CHARSET = "UTF-8";
+	private static String topic = "0.0.0.0_msgQueue";
+	private static XmlPullParserFactory factory = null;
 
-	public void initJafka() {
+	private static XMPPPacketReader reader = null;
+	private Thread thread;
+
+	private Producer<String, String> producer;
+	private ConsumerConnector connector;
+	private boolean flag = true;
+
+	static {
+		try {
+			factory = XmlPullParserFactory.newInstance(MXParser.class.getName(), null);
+			reader = new XMPPPacketReader();
+			reader.setXPPFactory(factory);
+		} catch (XmlPullParserException e) {
+			logger.error("Error creating a parser factory", e);
+		}
+	}
+
+	@Override
+	public void initializePlugin(PluginManager manager, File pluginDirectory) {
+		initJafkaProductor();
+		initJafkaConsumer();
+		OfflineMessageStrategy.addListener(this);
+
+		thread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				receiveMsgs();
+			}
+		});
+
+		thread.start();
+
+		System.out.println("jafka ok...");
+	}
+
+	@Override
+	public void destroyPlugin() {
+		destroyJafkaProductor();
+		destroyJafkaConsumer();
+
+		flag = false;
+
+		OfflineMessageStrategy.removeListener(this);
+	}
+
+	public void initJafkaProductor() {
 		Properties props = new Properties();
 		props.put("zk.connect", "192.168.150.83:2181");
 		props.put("serializer.class", StringEncoder.class.getName());
@@ -44,24 +106,88 @@ public class JafkaPlugin extends PluginAdaptor implements Plugin, OfflineMessage
 		//
 	}
 
-	public void destroyJafka() {
-		producer.close();
+	public void initJafkaConsumer() {
+		Properties props = new Properties();
+		props.put("zk.connect", "192.168.150.83:2181");
+		props.put("groupid", "test_group"+System.currentTimeMillis());
+		//
+		ConsumerConfig consumerConfig = new ConsumerConfig(props);
+		connector = Consumer.create(consumerConfig);
 	}
 
-	public void initializePlugin(PluginManager manager, File pluginDirectory) {
-		initJafka();
-		OfflineMessageStrategy.addListener(this);
-		System.out.println("jafka ok...");
+	public void destroyJafkaConsumer() {
+		if (connector != null) {
+			try {
+				connector.close();
+			} catch (IOException e) {
+				logger.error("destroyJafkaConsumer error!", e);
+			}
+		}
 	}
 
-	public void destroyPlugin() {
-		destroyJafka();
-		OfflineMessageStrategy.removeListener(this);
+	public void destroyJafkaProductor() {
+		if (producer != null) {
+			producer.close();
+		}
+	}
+
+	private void receiveMsgs() {
+		final PacketRouter packetRouter = XMPPServer.getInstance().getPacketRouter();
+		final ExecutorService executor = Executors.newFixedThreadPool(2);
+		while (flag) {
+			Map<String, List<MessageStream<String>>> topicMessageStreams = connector.createMessageStreams(
+					ImmutableMap.of(topic, 2), new StringDecoder());
+			List<MessageStream<String>> streams = topicMessageStreams.get(topic);
+			//
+
+			final AtomicInteger count = new AtomicInteger();
+			for (final MessageStream<String> stream : streams) {
+				executor.submit(new Runnable() {
+					public void run() {
+
+						Packet packet = null;
+
+						for (String message : stream) {
+							Element ele = null;
+
+							try {
+								ele = DocumentHelper.parseText(message).getDocument().getRootElement();
+							} catch (DocumentException e) {
+								logger.error("parseText error! {}", message, e);
+								continue;
+							}
+
+							if (message.startsWith("<message")) {
+								packet = new Message(ele);
+							} else if (message.startsWith("<presence")) {
+								packet = new Presence(ele);
+							} else {
+								logger.warn("unexpected package:{}", message);
+							}
+
+							if (packet != null) {
+								packet.getElement().addAttribute("mc", "1");
+							}
+
+							packetRouter.route(packet);
+
+							System.out.println(count.incrementAndGet() + " => " + message);
+						}
+					}
+				});
+			}
+		}
 	}
 
 	private boolean sendToJafka(Message message) {
 		try {
-			StringProducerData data = new StringProducerData("0.0.0.0_msgQueue");
+
+			if (message.getElement().attribute("mc") != null) {
+				//存离线
+				return true;
+			}
+
+			StringProducerData data = new StringProducerData(topic);
 
 			//同时发多个
 			data.add(message.toXML());
@@ -87,31 +213,6 @@ public class JafkaPlugin extends PluginAdaptor implements Plugin, OfflineMessage
 
 	public static void main(String[] args) throws Exception {
 
-		Properties props = new Properties();
-		props.put("zk.connect", "192.168.150.83:2181");
-		props.put("groupid", "test_group");
-		//
-		ConsumerConfig consumerConfig = new ConsumerConfig(props);
-		ConsumerConnector connector = Consumer.create(consumerConfig);
-		//
-		Map<String, List<MessageStream<String>>> topicMessageStreams = connector.createMessageStreams(
-				ImmutableMap.of("demo", 2), new StringDecoder());
-		List<MessageStream<String>> streams = topicMessageStreams.get("demo");
-		//
-		ExecutorService executor = Executors.newFixedThreadPool(2);
-		final AtomicInteger count = new AtomicInteger();
-		for (final MessageStream<String> stream : streams) {
-			executor.submit(new Runnable() {
-
-				public void run() {
-					for (String message : stream) {
-						System.out.println(count.incrementAndGet() + " => " + message);
-					}
-				}
-			});
-		}
-		//
-		executor.awaitTermination(1, TimeUnit.HOURS);
 	}
 
 	@Override
